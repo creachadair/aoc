86a3e5d955f14974f7bcce0983085c9855e5667d
M. J. Fromberger <fromberger@tailscale.com>
2022-12-07 11:48:57 -0800

    2022-07: add solution part2

---
+++ 2022/07/part2/solve.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import os, sys
+input_name = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+
+def parse(lines):
+    cwd = []
+    def path(*rest):
+        return '/' + '/'.join(cwd+list(rest))
+
+    smap = {}  # path -> size, directs only
+    dmap = {}  # path -> [dir], directs only
+    for line in lines:
+        tag, name, *rest = line.split()
+        if tag == '$':
+            if name == 'cd':
+                if rest[0] == '/':
+                    cwd = []
+                elif rest[0] == '..':
+                    cwd.pop()
+                else:
+                    cwd.append(rest[0])
+                dmap.setdefault(path(), [])
+                smap.setdefault(path(), 0)
+            elif name != 'ls':
+                raise KeyError(name)
+        elif tag == 'dir':
+            dmap[path()].append(path(name))
+        else:
+            size = int(tag)
+            smap[path()] += size
+
+    class tree(object):
+        def sizeof(self, path):
+            size = smap[path]
+            for sub in dmap[path]:
+                size += self.sizeof(os.path.join(path, sub))
+            return size
+        def dirs(self):
+            return smap.keys()
+
+    return tree()
+
+
+with open(input_name, 'r') as fp:
+    tree = parse(fp.read().strip().split('\n'))
+
+total = 70000000  # from puzzle.txt
+needs = 30000000  # from puzzle.txt
+inuse = tree.sizeof('/')
+avail = total - inuse
+
+for size, path in sorted((tree.sizeof(path), path) for path in tree.dirs()):
+    if avail + size >= needs:
+        print(path, size)
+        break

492edae2cc62ebf498fc5168d5802c669fb82bdf
M. J. Fromberger <fromberger@tailscale.com>
2022-12-25 12:54:56 -0800

    2022-19: add -lo and -hi flags to part1

--- 2022/19/part1/solve.go
+++ 2022/19/part1/solve.go
@@ -15,6 +15,8 @@ var (
 	inputFile  = flag.String("input", "input.txt", "Input file path")
 	maxMinutes = flag.Int("minutes", 24, "Maximum minutes to run")
 	maxTasks   = flag.Int("tasks", 8, "Concurrent tasks")
+	loPlan     = flag.Int("lo", 0, "First plan to simulate")
+	hiPlan     = flag.Int("hi", 0, "Last plan to simulate")
 	doVerbose  = flag.Bool("v", false, "Verbose logging")
 )
 
@@ -28,6 +30,10 @@ func main() {
 
 	var total int64
 	for _, plan := range ps {
+		if (*loPlan > 0 && plan.ID < *loPlan) || (*hiPlan > 0 && plan.ID > *hiPlan) {
+			continue
+		}
+
 		s := robot.NewState(plan, *maxMinutes)
 		run(func() error {
 			log.Printf("Begin solving plan %d", s.Plan.ID)
@@ -36,7 +42,7 @@ func main() {
 			score := stats.Best * s.Plan.ID
 			fmt.Printf("Plan %d yields maximum %d geodes, score=%d\n",
 				s.Plan.ID, stats.Best, score)
-			fmt.Println(stats)
+			fmt.Println("-", stats)
 			atomic.AddInt64(&total, int64(score))
 
 			if *doVerbose {

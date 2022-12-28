7f228f4ab6508b1314f2a4c872d843631248be20
M. J. Fromberger <fromberger@tailscale.com>
2022-12-28 00:21:51 -0800

    2022-21: add puzzle statement and solution part2

---
+++ 2022/21/part2/solve.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"aoc/2022/21/rules"
+)
+
+var (
+	inputFile = flag.String("input", "input.txt", "Input file path")
+	rootNode  = flag.String("root", "root", "Name of the root")
+	varNode   = flag.String("var", "humn", "Name of the variable node")
+)
+
+func main() {
+	flag.Parse()
+
+	rs := rules.MustParse(*inputFile)
+	g := rules.NewGraph(rs)
+	g.Rules[*rootNode].Op = "=" // mark the root as an equality test
+	g.Rules[*varNode].Op = "?"  // mark the variable node as unresolved
+	g.PreSolve()
+
+	v, err := g.Solve(*rootNode, 0)
+	if err != nil {
+		log.Fatalf("Solve %q failed: %v", *rootNode, err)
+	}
+
+	fmt.Println("Root:", int(v))
+	fmt.Println("Var:", int(g.Values[*varNode]))
+}

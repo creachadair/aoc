9447d84d91dac769efc199c30e7da391347ccd18
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-12-14 21:34:59 -0800

    2023-15: add part2 puzzle and solution

--- 2023/15/part2/solve.go
+++ 2023/15/part2/solve.go
@@ -4,18 +4,68 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	lib "github.com/creachadair/aoc/2023/15"
 	"github.com/creachadair/aoc/aoc"
 )
 
-var _ = lib.OK
+var _ = lib.Hash
 
 func main() {
 	flag.Parse()
-	m, err := aoc.ParseMap(aoc.MustReadLines())
-	if err != nil {
-		log.Fatalf("Parse map: %v", err)
+	input := strings.TrimSpace(string(aoc.MustReadInput()))
+	rules := strings.Split(input, ",")
+
+	type lens struct {
+		label string
+		focal byte
 	}
-	fmt.Println(m)
+	box := make([][]lens, 256)
+	remove := func(label string) {
+		h := lib.Hash(label)
+		for j := 0; j < len(box[h]); j++ {
+			if box[h][j].label == label {
+				box[h] = append(box[h][:j], box[h][j+1:]...)
+				return
+			}
+		}
+	}
+	replace := func(label string, focal int) {
+		h := lib.Hash(label)
+		for j := 0; j < len(box[h]); j++ {
+			if box[h][j].label == label {
+				box[h][j] = lens{label: label, focal: byte(focal)}
+				return
+			}
+		}
+		box[h] = append(box[h], lens{label: label, focal: byte(focal)})
+	}
+
+	for i, rule := range rules {
+		label, focal, ok := strings.Cut(rule, "=")
+		if !ok {
+			remove(strings.TrimSuffix(label, "-"))
+		} else {
+			v, err := strconv.Atoi(focal)
+			if err != nil {
+				log.Fatalf("Rule %d (%q): invalid focal length: %v", i+1, rule, err)
+			}
+			replace(label, v)
+		}
+	}
+
+	var totalPower int
+	for i, b := range box {
+		var boxPower int
+		for j, e := range b {
+			boxPower += (i + 1) * (j + 1) * int(e.focal)
+		}
+		if boxPower != 0 {
+			log.Printf("Box %d: power=%d", i+1, boxPower)
+		}
+		totalPower += boxPower
+	}
+	fmt.Println(totalPower)
 }
